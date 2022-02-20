911297830ad5cdeb70b17185bf88d14b8a46f36a
Yapi Donatien Achou <yapi@Yapis-MacBook-Pro.local>
2022-02-20 16:51:57 +0100

    global updates

---
+++ examples/mlp.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import Input, Model
+
+
+def mpl_sequential_api_v1() -> Sequential:
+    """Implements a sequential api.
+    """
+    model = Sequential([
+        ])
+    return model
+
+
+def mlp_functional_api(input_shape: tuple) -> Model:
+    """Implements a functional api.
+    """
+    input_image = Input(shape=input_shape)
+
+
+if __name__ == '__main__':
+    
+    model = mlp_functional_api()
+    print(model.summary())
+
+    tf.keras.utils.plot_model(model, to_file='model.png')
